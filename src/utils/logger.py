1b5ac39709ad2c2886cca932e3114ff9a584a6bf
Ayush <ayusharyaneth@gmail.com>
2026-03-08 00:27:45 +0530

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,19 +1,19 @@
+# src/utils/logger.py
 import structlog
 import logging
-from config.settings import LOG_LEVEL
 
-structlog.configure(
-    processors=[
-        structlog.stdlib.add_log_level,
-        structlog.stdlib.PositionalArgumentsFormatter(),
-        structlog.processors.TimeStamper(fmt="iso"),
-        structlog.processors.JSONRenderer()
-    ],
-    context_class=dict,
-    logger_factory=structlog.stdlib.LoggerFactory(),
-    wrapper_class=structlog.stdlib.BoundLogger,
-    cache_logger_on_first_use=True,
-)
-
-logging.basicConfig(format="%(message)s", level=LOG_LEVEL)
-logger = structlog.get_logger()
+def get_logger(name: str):
+    structlog.configure(
+        processors=[
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.JSONRenderer()
+        ],
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+    logging.basicConfig(format="%(message)s", level=logging.INFO)
+    return structlog.get_logger(name)
