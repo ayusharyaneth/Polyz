d3b06198d121b975b196a9b602ad5fd53b5c3cb6
Ayush <ayusharyaneth@gmail.com>
2026-03-07 18:42:55 +0530

    Create logger.py

---
+++ src/utils/logger.py
@@ -0,0 +1,19 @@
+import structlog
+import logging
+from config.settings import LOG_LEVEL
+
+structlog.configure(
+    processors=[
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.JSONRenderer()
+    ],
+    context_class=dict,
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    cache_logger_on_first_use=True,
+)
+
+logging.basicConfig(format="%(message)s", level=LOG_LEVEL)
+logger = structlog.get_logger()
