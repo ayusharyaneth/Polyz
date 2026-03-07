572910c2fb395d1c945ccb4db5488ce120408312
Ayush <ayusharyaneth@gmail.com>
2026-03-08 01:38:08 +0530

    Update trade_controller.py

--- src/controllers/trade_controller.py
+++ src/controllers/trade_controller.py
@@ -1,5 +1,5 @@
 # src/controllers/trade_controller.py
-from src.database.db import db_pool
+from src.database.db import db_instance
 
 async def get_positions(user_id: int):
-    return await db_pool.fetch("SELECT * FROM positions WHERE user_id = $1", user_id)
+    return await db_instance.db.positions.find({"user_id": user_id}).to_list(length=None)
