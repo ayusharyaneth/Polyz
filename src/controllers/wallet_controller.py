ff24a8b5d2b2758709ad62384576be841de5a482
Ayush <ayusharyaneth@gmail.com>
2026-03-08 01:38:23 +0530

    Update wallet_controller.py

--- src/controllers/wallet_controller.py
+++ src/controllers/wallet_controller.py
@@ -1,8 +1,9 @@
 # src/controllers/wallet_controller.py
-from src.database.db import db_pool
+from src.database.db import db_instance
 
 async def add_target_wallet(user_id: int, address: str, label: str):
-    await db_pool.execute(
-        "INSERT INTO target_wallets (user_id, address, label) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING",
-        user_id, address, label
+    await db_instance.db.target_wallets.update_one(
+        {"user_id": user_id, "address": address},
+        {"$set": {"label": label}},
+        upsert=True
     )
