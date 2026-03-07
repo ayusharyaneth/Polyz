29e549aca04f929800a6dba3c8faf46d4fb2206e
Ayush <ayusharyaneth@gmail.com>
2026-03-08 01:37:51 +0530

    Update settings_controller.py

--- src/controllers/settings_controller.py
+++ src/controllers/settings_controller.py
@@ -1,5 +1,8 @@
 # src/controllers/settings_controller.py
-from src.database.db import db_pool
+from src.database.db import db_instance
 
 async def update_copy_percentage(user_id: int, percentage: float):
-    await db_pool.execute("UPDATE bot_settings SET copy_percentage = $1 WHERE user_id = $2", percentage, user_id)
+    await db_instance.db.bot_settings.update_one(
+        {"user_id": user_id}, 
+        {"$set": {"copy_percentage": percentage}}
+    )
